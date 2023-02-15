2c48d7991db68e25a13e0109c97c7ffbdd1e771c
tblake767 <125328962+tblake767@users.noreply.github.com>
2023-02-15 18:45:22 -0500

    Created repo1.sh
    Basic test script using if/then

---
+++ repo1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "What class are you in now: " class
+
+#Test script from online repo
+if [ class = Linux ]
+then
+  echo "Welcome to tonight's class. We are covering Github."
+fi
